e13e9e322baf52c016ef0ba008e8e56498f9b055
Chelos <chan022@outlook.com>
2021-08-23 05:03:54 +0700

    x

--- register.js
+++ register.js
@@ -4,8 +4,34 @@ const qs = require('qs');
 
 const md5 = (s) => crypto.createHash('md5').update(s).digest('hex');
 
+function getRandomInRange(from, to, fixed) {
+    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+}
+
+async function GetSessionID() {
+    let res = await fetch("https://member.naelike.com/getbeamed",{
+        follow: 0
+    })
+    let cookie = (res.headers.get('set-cookie')||'').match(/(?<=_S_e_Ss_Io_N-__NlSesS=)[\w\d]+/)
+    if(!cookie) throw Error("CF_MAYBE?")
+    return cookie[0]
+}
+
+function GenerateFingerprint() {
+    let latitude = getRandomInRange(-180, 180, 4),
+        longitude = getRandomInRange(-180, 180, 4)
+
+    let location=latitude+','+longitude+'_S_e_Ss_Io_N-__NlMmb';
+    let md5_location=md5(location);
+
+    return {
+        cookie : `_se__SiO_n_gPsIn=${md5_location}; Us_E_R_Nam_e_GpsIN=${md5(md5_location+'1')}; Ss_eNNAS_GpsIN=${md5(md5_location+'2')}; GP_s_lL_GpsIN=${md5(md5_location+'3')};`,
+        latitude,longitude
+    }
+}
+
 async function register(username,email,password,phone) {
-    const register_fingerprint = await GenerateLoginFingerprint()
+    const register_fingerprint = await GenerateFingerprint()
     const session_id = await GetSessionID()
     return fetch("https://member.naelike.com/register/do_register",{
         "method": "POST",
@@ -22,26 +48,6 @@ async function register(username,email,password,phone) {
             gps: `${register_fingerprint.latitude},${register_fingerprint.longitude}`
         }),
     }).then(res => res.text()).then(console.log)
-}
-async function GetSessionID() {
-    let res = await fetch("https://member.naelike.com/getbeamed",{
-        follow: 0
-    })
-    let cookie = (res.headers.get('set-cookie')||'').match(/(?<=_S_e_Ss_Io_N-__NlSesS=)[\w\d]+/)
-    if(!cookie) throw Error("CF_MAYBE?")
-    return cookie[0]
-}
-function GenerateLoginFingerprint() {
-    let latitude = Math.random()
-    let longitude = Math.random()
-
-    let location=latitude+','+longitude+'_S_e_Ss_Io_N-__NlMmb';
-    let md5_location=md5(location);
-
-    return {
-        cookie : `_se__SiO_n_gPsIn=${md5_location}; Us_E_R_Nam_e_GpsIN=${md5(md5_location+'1')}; Ss_eNNAS_GpsIN=${md5(md5_location+'2')}; GP_s_lL_GpsIN=${md5(md5_location+'3')};`,
-        latitude,longitude
-    }
 }
 
 register("chanios","chanios@github.com","chadasdanios1234","0584930213")
